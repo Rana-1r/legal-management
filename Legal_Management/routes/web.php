1bd1a088f27cfea83eebd27aabe5a9d67e7f40c1
salam02-smh <salamhariri298@gmail.com>
2026-05-05 15:08:51 +0300

    Update web.php

--- Legal_Management/routes/web.php
+++ Legal_Management/routes/web.php
@@ -65,26 +65,11 @@ Route::get('/manager-interface', [ConsultationController::class, 'managerIndex']
 Route::post('/assign-task', [ConsultationController::class, 'storeTask'])
     ->name('tasks.assign');
 
-<<<<<<< HEAD
-// صفحة الاستشارات للموظف القانوني  
-=======
-<<<<<<< HEAD
-
-Route::get('/manager-interface', [ConsultationController::class, 'managerIndex'])
-    ->name('manager.interface');
-
 // صفحة الموظف القانوني للإستشارات
->>>>>>> 6fa69bc580e874ba2faa14bbfac7c20b959d70bf
 Route::get('/employee/legal', [ConsultationController::class, 'legalEmployeePage'])
     ->name('employee.legal');
 
 // لوحة تحكم الموظف القانوني 
 Route::get('/employee/interface', [ConsultationController::class, 'employeeInterfacePage'])
     ->name('employee.interface');
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 463ba191e023cfcbb99ce9910a12e5f91bcd8701
->>>>>>> 6fa69bc580e874ba2faa14bbfac7c20b959d70bf
 });
