6b71f2992df9b3b04ee74c7891eca02f9c916828
Rana-1r <rnoo00890@gmail.com>
2026-05-06 09:53:22 +0300

    y

--- Legal_Management/routes/web.php
+++ Legal_Management/routes/web.php
@@ -17,74 +17,59 @@ Route::post('/', [LoginController::class, 'login']);
 
 // كل صفحات المنصة محمية
 Route::middleware('auth')->group(function () {
- 
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-Route::post('/profile/update', [ProfileController::class, 'updateInfo'])->name('profile.update');
-Route::post('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
 
-/* مهم: لازم status يكون قبل resource */
-Route::get('/consultations/status', [ConsultationController::class, 'status'])
-    ->name('consultations.status');
+    // صفحة اليوزر الرئيسية
+    Route::get('/user-interface', [DashboardController::class, 'index'])
+        ->name('user-interface');
 
-/* عدلناها عشان ما ينادي show */
-Route::resource('consultations', ConsultationController::class)
-    ->except(['show']);
+    // البروفايل
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::post('/profile/update', [ProfileController::class, 'updateInfo'])->name('profile.update');
+    Route::post('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.photo.update');
 
-Route::get('/user-interface', [DashboardController::class, 'index'])
-    ->name('user-interface');
+    // صفحة خدمات الاستشارات عند اليوزر
+    Route::get('/consultations-page', [ConsultationController::class, 'userPage'])
+        ->name('consultations.page');
 
-Route::resource('consultations', ConsultationController::class);
-  
-Route::get('/my-consultations', [ConsultationController::class, 'my'])
-    ->name('consultations.my');
+    // صفحة طلب استشارة
+    Route::get('/consultations/request', [ConsultationController::class, 'create'])
+        ->name('consultations.create');
 
-Route::get('/legal-manager', [ConsultationController::class, 'managerPage'])
-    ->name('legal.manager');
+    // حفظ طلب الاستشارة
+    Route::post('/consultations/request', [ConsultationController::class, 'store'])
+        ->name('consultations.store');
 
-Route::post('/consultations/{id}/assign', [ConsultationController::class, 'assignLawyer'])
-    ->name('consultations.assign');
+    // استشاراتي
+    Route::get('/my-consultations', [ConsultationController::class, 'my'])
+        ->name('consultations.my');
 
-Route::get('/consultations-page', [ConsultationController::class, 'userPage'])
-    ->middleware('auth')
-    ->name('consultations.page');
+    // حالة الاستشارات
+    Route::get('/consultations/status', [ConsultationController::class, 'status'])
+        ->name('consultations.status');
 
-Route::post('/tasks/{id}/complete', [ConsultationController::class, 'completeTask'])
-    ->name('tasks.complete');
+    // صفحة المدير القانوني
+    Route::get('/legal-manager', [ConsultationController::class, 'managerPage'])
+        ->name('legal.manager');
 
-/* صفحة طلب استشارة */
-Route::get('/consultations/request', [ConsultationController::class, 'create'])
-    ->name('consultations.create');
+    Route::get('/manager-interface', [ConsultationController::class, 'managerIndex'])
+        ->name('manager.interface');
 
-/* حفظ طلب الاستشارة */
-Route::post('/consultations/request', [ConsultationController::class, 'store'])
-    ->name('consultations.store');
+    // تعيين محامي
+    Route::post('/consultations/{id}/assign', [ConsultationController::class, 'assignLawyer'])
+        ->name('consultations.assign');
 
-Route::get('/manager-interface', [ConsultationController::class, 'managerIndex'])
-    ->name('manager.interface');
+    // المهام
+    Route::post('/assign-task', [ConsultationController::class, 'storeTask'])
+        ->name('tasks.assign');
 
-Route::post('/assign-task', [ConsultationController::class, 'storeTask'])
-    ->name('tasks.assign');
+    Route::post('/tasks/{id}/complete', [ConsultationController::class, 'completeTask'])
+        ->name('tasks.complete');
 
-<<<<<<< HEAD
+    // الموظف القانوني
+    Route::get('/employee/legal', [ConsultationController::class, 'legalEmployeePage'])
+        ->name('employee.legal');
 
-Route::get('/manager-interface', [ConsultationController::class, 'managerIndex'])
-    ->name('manager.interface');
+    Route::get('/employee/interface', [ConsultationController::class, 'employeeInterfacePage'])
+        ->name('employee.interface');
 
-=======
->>>>>>> 1bd1a088f27cfea83eebd27aabe5a9d67e7f40c1
-// صفحة الموظف القانوني للإستشارات
-Route::get('/employee/legal', [ConsultationController::class, 'legalEmployeePage'])
-    ->name('employee.legal');
-
-// لوحة تحكم الموظف القانوني 
-Route::get('/employee/interface', [ConsultationController::class, 'employeeInterfacePage'])
-    ->name('employee.interface');
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-
-=======
->>>>>>> f5dcdded92b7fc5677a7310eb05251d7865e4e3b
-=======
->>>>>>> 1bd1a088f27cfea83eebd27aabe5a9d67e7f40c1
 });
